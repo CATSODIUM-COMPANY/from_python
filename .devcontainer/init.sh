ba34d31d8160c9b4c8866bb238cdc796203b0142
Thitiwat Kosolpattanadurong <167766536+CATISNOTSODIUM@users.noreply.github.com>
2024-06-05 10:52:26 +0700

    Add .devcontainer

---
+++ .devcontainer/init.sh
@@ -0,0 +1,10 @@
+
+#Docker hardening
+#remove pip (for python)
+
+echo "Hi"
+rm -rf .devcontainer
+
+#alias apk='echo "You cannot install external packages."'
+
+#export ENV=/etc/global_aliases.sh sh 
